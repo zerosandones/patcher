58de76e30e16ee0b39fa340510b25e3d2a054052
Dave Glendenning <zerosandones@users.noreply.github.com>
2024-10-13 09:59:26 +0000

    feat: :sparkles: Allow for the adding of all Json data types
    Allows for adding all json data types. Setup intergration tests instead of unit tests.

--- src/main/java/dev/zerosandones/patcher/Patcher.java
+++ src/main/java/dev/zerosandones/patcher/Patcher.java
@@ -1,16 +1,24 @@
 package dev.zerosandones.patcher;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.bson.BsonArray;
+import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
+import org.bson.BsonInt32;
+import org.bson.BsonNull;
 import org.bson.BsonString;
+import org.bson.BsonValue;
 
+import jakarta.json.JsonNumber;
 import jakarta.json.JsonPatch;
+import jakarta.json.JsonString;
 import jakarta.json.JsonValue;
+import jakarta.json.JsonValue.ValueType;
+
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.List;
 
 public class Patcher {
-
-    private static final Logger logger = LogManager.getLogger(Patcher.class);
 
     public BsonDocument generateBson(JsonPatch patch) {
         BsonDocument doc = new BsonDocument();
@@ -18,15 +26,95 @@ public class Patcher {
             .map(JsonValue::asJsonObject)
             .forEach(patchObject -> {
                 String mongoOp = "";
+                
                 String operation = patchObject.getString("op");
-                String path = patchObject.getString("path").substring(1).replace('/', '.');
-                String value = patchObject.getString("value");
+                PointerPath path = new PointerPath(patchObject.getString("path"));
+
+                BsonValue value = generateValue(patchObject.get("value"));
+
                 if ("add".equals(operation)) {
-                    mongoOp = "$add";
+                    if (path.doesTargetArray()) {
+                        mongoOp = "$push";
+                        BsonArray array = new BsonArray();
+                        array.add(value);
+                        BsonDocument valueDoc = new BsonDocument("$each", array);
+                        if (path.getIndex() != null) {
+                            valueDoc.put("$position", new BsonInt32(path.getIndex()));
+                        }
+                        value = valueDoc;
+                    } else {
+                        mongoOp = "$set";
+                    }
                 }
-                doc.computeIfAbsent(mongoOp, key -> new BsonDocument()).asDocument().put(path, new BsonString(value));
+                doc.computeIfAbsent(mongoOp, key -> new BsonDocument()).asDocument().put(path.getPath(), value);
             });
         return doc;
+    }
+
+    private BsonValue generateValue(JsonValue value) {
+
+        BsonValue generatedValue = null;
+        if (value.getValueType() == ValueType.STRING) {
+            generatedValue = new BsonString(((JsonString)value).getString());
+        } else if (value.getValueType() == ValueType.NUMBER) { //TODO: deal with different number types
+            generatedValue = new BsonInt32(((JsonNumber)value).intValueExact());
+        } else if (value.getValueType() == ValueType.OBJECT) {
+            generatedValue = BsonDocument.parse(value.asJsonObject().toString());
+        } else if (value.getValueType() == ValueType.FALSE ) {
+            generatedValue = new BsonBoolean(false);
+        } else if (value.getValueType() == ValueType.TRUE) {
+            generatedValue = new BsonBoolean(true);
+        } else if (value.getValueType() == ValueType.NULL) {
+            generatedValue = new BsonNull();
+        } else if (value.getValueType() == ValueType.ARRAY) {
+            List<BsonValue> arrayData = value.asJsonArray().stream()
+                .map(this::generateValue)
+                .collect(Collectors.toList());
+                generatedValue = new BsonArray(arrayData);
+        }
+
+        return generatedValue;
+
+    }
+
+    private class PointerPath {
+
+        private Pattern pattern = Pattern.compile("\\d"); //checks for a number, which is used to show array index
+
+        private String path;
+        private boolean targetsArray = false;
+        private Integer index;
+
+        protected PointerPath(String path) {
+            
+            this.path = path.substring(1).replace('/', '.');
+            int lastIndex = this.path.lastIndexOf(".");
+            if (lastIndex >= 0) {
+                String lastPathSection = this.path.substring(lastIndex + 1);
+                if ("-".equals(lastPathSection)) {
+                    this.targetsArray = true;
+                    this.path = this.path.substring(0, lastIndex);
+                } else if (pattern.matcher(lastPathSection).matches()) {
+                    this.targetsArray = true;
+                    this.index = Integer.parseInt(lastPathSection);
+                    this.path = this.path.substring(0, lastIndex);
+                }
+            }
+            
+        }
+
+        public boolean doesTargetArray() {
+            return targetsArray;
+        }
+
+        public Integer getIndex() {
+            return this.index;
+        }
+
+        public String getPath() {
+            return this.path;
+        }
+
     }
     
 }
